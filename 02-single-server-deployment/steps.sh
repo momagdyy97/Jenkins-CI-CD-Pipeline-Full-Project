915161a938f75ea52e80e7eaaef7a32650291ec3
root <root@ip-172-31-16-254.eu-west-2.compute.internal>
2025-05-03 12:42:23 +0000

    lastest updates

--- 02-single-server-deployment/steps.sh
+++ 02-single-server-deployment/steps.sh
@@ -1,23 +1,19 @@
 mkdir app
 python3 -m venv .venv
+pm2 start venv/bin/python3 --name flask-app -- app.py
+#[Unit]
+#Description=flask app
+#After=network.target
+#[Service]
+#User=ec2-user
+#Group=ec2-user
+#WorkingDirectory=/home/ec2-user/app/
+#Environment="PATH=/home/ec2-user/app/venv/bin"
+#ExecStart=/home/ec2-user/app/venv/bin/python3 /home/ec2-user/app/app.py
+#[Install]
+#WantedBy=multi-user.target
+#sudo vi flaskapp.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable flaskapp.service
+#sudo systemctl start flaskapp.service
 
-
-[Unit]
-Description=flask app
-After=network.target
-
-[Service]
-User=ec2-user
-Group=ec2-user
-WorkingDirectory=/home/ec2-user/app/
-Environment="PATH=/home/ec2-user/app/venv/bin"
-ExecStart=/home/ec2-user/app/venv/bin/python3 /home/ec2-user/app/app.py
-
-[Install]
-WantedBy=multi-user.target
-
-
-sudo vi flaskapp.service
-sudo systemctl daemon-reload
-sudo systemctl enable flaskapp.service
-sudo systemctl start flaskapp.service
